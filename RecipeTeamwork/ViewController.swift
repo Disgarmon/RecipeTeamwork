2831aa8df145a0e82bdb0f17bd8c010911c86b30
Daniil Lebedev <disgarmonvideo@gmail.com>
2022-07-26 14:38:45 +0300

    Initial Commit

---
+++ RecipeTeamwork/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RecipeTeamwork
+//
+//  Created by Daniil Lebedev on 26.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
